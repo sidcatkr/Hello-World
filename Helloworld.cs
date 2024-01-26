a74db34edcbf69a2a93f3f5a14d446c6936affd1
sidcatkr <82746617+sidcatkr@users.noreply.github.com>
2024-01-27 02:36:01 +0900

    Create Helloworld.cs

---
+++ Helloworld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace asdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
